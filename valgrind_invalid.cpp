44cc451f0073ccb063107046d77076fe2b3c6d68
Seth Cooper <seth.cooper@gmail.com>
2020-10-02 13:07:45 -0400

    Updates.

--- valgrind_invalid.cpp
+++ valgrind_invalid.cpp
@@ -7,7 +7,10 @@ int main() {
 	vector<int> v = { 1, 2, 3, 4 };
 	v[4] = 5;
 
+	int x = v[4];
+	/*
 	string s = "hello!";
-	char c = s[7];
-	cout << c << endl;
+	char x = s[7];
+	*/
+	cout << x << endl;
 }
